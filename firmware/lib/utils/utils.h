400e77b7773c87411852cf069ea89b296b1ce06a
Roberto Albesiano <roberto.albesiano@gmail.com>
2016-05-02 15:19:26 +0200

    Improved compression algorithm

--- firmware/lib/utils/utils.h
+++ firmware/lib/utils/utils.h
@@ -26,33 +26,33 @@ inline void digitalPotWrite(int address, int value) {
 }
 
 // ---------------------------------------------------------
-// power operator helper function
-inline int power(int base, int exp)
-{
-    int result = 1;
-    while(exp) {
-        result *= base;
-        exp--;
-    }
-    return result;
-}
-
 // bool array to byte converter
-inline byte bool2byte(boolean* input, int dim = 6) {
-  byte output = 0;
-  for(int i = 0; i < dim; i++)
-      output += input[i] * power(2,i);
+inline int bool2byte(bool* input, int dim = 6) {
+
+  unsigned int output = 0;
+
+  // bitwise conversion (see http://http://playground.arduino.cc/Code/BitMath)
+  for(int i = 0; i < dim; i++) {
+    // if input[i] is true, toggles output i-th byte (actually,
+    // as output is initially 0, it sets its i-th byte to true)
+    if(input[i]) output = output ^ (1<<i);
+  }
+
   return output;
+
 }
 
 // byte to bool array converter
-inline boolean* byte2bool(byte input, int dim = 6) {
-  boolean* output = new boolean[dim];
-  for(int i = 0; i < dim; i++){
-    output[i] = input % 2;
-    input = (input - output[i])/2;
-  }
+inline bool* byte2bool(unsigned int input, int dim = 6) {
+
+  bool* output = new bool[dim];
+
+  // bitwise conversion (see http://http://playground.arduino.cc/Code/BitMath)
+  // sets output[i] to i-th byte
+  for(int i = 0; i < dim; i++) output[i] = input & (1<<i);
+
   return output;
+
 }
 
 #endif
